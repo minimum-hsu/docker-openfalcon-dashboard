508732c9033678367a1064d23e7c90351edec5a3
Minimum <s5550055@yahoo.com.tw>
2015-08-24 11:04:24 +0800

    Replace "bash" with "tail".

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ cp $CONFIGDIR/$CONFIGFILE $WORKDIR/rrd/$CONFIGFILE
 virtualenv $WORKDIR/env
 pip install -r /home/dashboard/pip_requirements.txt
 $WORKDIR/control restart
-/bin/bash
+$WORKDIR/control tail
